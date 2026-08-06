6713f1778c23c72fdb8bea9719a8b8589b26b2cf
Anushka <anushkareddy1208@gmail.com>
2026-08-06 10:52:58 +0530

    correct path

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const authRoutes = require("./routes/auth.routes");
+
 const app = express();
 
 app.use(express.json());
@@ -9,5 +11,7 @@ app.get("/", (req, res) => {
         message: "Welcome to AnuDB Backend 🚀"
     });
 });
+
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
